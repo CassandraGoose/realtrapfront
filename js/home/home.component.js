6248dce6be6e7e4cfd1fe11ee49149721ca3bde0
Cassandra Torske <cassandra.torske@gmail.com>
2017-05-30 13:29:20 -0600

    things

--- js/home/home.component.js
+++ js/home/home.component.js
@@ -14,7 +14,7 @@ function HomeController($http, $stateParams, $state, $timeout) {
   function playSound() {
     console.log("button");
     vm.showButton = false
-    $http.get("https://192.168.1.12:4000/")
+    $http.get("http://10.6.65.133:4000/")
       .then(function() {
         $timeout(function(){
           vm.showButton = true
